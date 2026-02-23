e12380fe5fe24a8cae5dba1a8d26671c92631840
Ayush <ayush642001@gmail.com>
2026-02-24 01:04:19 +0530

    data ingestion

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from src.DataScienceProject.logger import logging
 from src.DataScienceProject.exception import CustomException
+from src.DataScienceProject.components.data_ingestion import DataIngestion
+from src.DataScienceProject.components.data_ingestion import DataIngestionConfig
 import sys 
 
 if __name__ == "__main__":
     logging.info("This is a log message from the main block.")
     try:
-        a = 1 / 0  # This will raise a ZeroDivisionError
+        data_ingestion = DataIngestion()
+        # data_ingestion_config = DataIngestionConfig()
+        data_ingestion.initiate_data_ingestion()
+
+
+
     except Exception as e:
         logging.info("Custom exception occurred.")
         raise CustomException(str(e), sys)
---
+++ src/DataScienceProject/utils.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from src.DataScienceProject.exception import CustomException
+from src.DataScienceProject.logger import logging
+import pandas as pd
+from dotenv import load_dotenv
+import pymysql
+
+load_dotenv()
+
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+db = os.getenv("db")
+
+def read_sql_data():
+    logging.info("Reading SQL database started")
+
+    try:
+        mydb = pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            db=db
+        )
+        logging.info(f"Connection Established: \n{mydb}")
+        df = pd.read_sql_query("Select * from inventory", mydb)
+        print(df.head())
+
+        return df
+
+    except Exception as e:
+        raise CustomException(e)
